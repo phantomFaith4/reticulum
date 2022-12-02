2e964cf0c30c7c109a555e7ccdf56095dd69bf3a
phantomFaith4 <phantomFaith4@gmail.com>
2022-12-02 13:37:25 +0100

    reticulum api postgrest calls

--- lib/ret_web/router.ex
+++ lib/ret_web/router.ex
@@ -92,7 +92,9 @@ defmodule RetWeb.Router do
   end
 
   scope "/api/postgrest" do
-    pipe_through([:secure_headers, :auth_required, :admin_required, :proxy_api])
+    if(Mix.env() == :prod) do
+      pipe_through([:secure_headers])
+    end
     forward("/", RetWeb.Plugs.PostgrestProxy)
   end
 
