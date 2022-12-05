0969386b702b757dc3943f3d6c90fb394e1af597
phantomFaith4 <phantomFaith4@gmail.com>
2022-12-05 12:24:10 +0100

    m

--- lib/ret_web/plugs/proxies.ex
+++ lib/ret_web/plugs/proxies.ex
@@ -8,6 +8,7 @@ defmodule RetWeb.Plugs.PostgrestProxy do
     opts = ReverseProxyPlug.init(upstream: "http://#{hostname()}:3000")
     ReverseProxyPlug.call(conn, opts)
     IO.inspect(hostname(),label: "hostname fun =>")
+    IO.puts "Hello word from media url 9"
   end
 
   @spec hostname :: String.t()
@@ -21,4 +22,5 @@ end
 defmodule RetWeb.Plugs.ItaProxy do
   use Plug.Builder
   plug ReverseProxyPlug, upstream: "http://localhost:6000"
+  IO.puts "Hello word from media url 10"
 end
