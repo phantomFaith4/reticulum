3f6ec7954255aef6ff36e04727b84037c7c25dfc
phantomFaith4 <phantomFaith4@gmail.com>
2022-12-05 08:45:10 +0100

    media resolver

--- lib/ret/media_resolver.ex
+++ lib/ret/media_resolver.ex
@@ -120,8 +120,10 @@ defmodule Ret.MediaResolver do
 
       resolved_ip ->
         if HttpUtils.internal_ip?(resolved_ip) do
+          resolvedMedia = resolve_with_content_type(query)
           {:commit, resolvedMedia}
-          #:forbidden
+
+          # :forbidden
         else
           resolve_with_ytdl(query, root_host, query |> ytdl_format(root_host))
         end
